6ff15a0f0759bd6e824e84444308784e03c2a60e
wyattfry <wyattfry@gmail.com>
2018-02-06 11:41:21 -0800

    Allows messages to be displayed in reverse chrono order

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,11 +11,16 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var RETURN_LIMIT = 5;
 var url = require('url');
 var messages = [];
 var data = {
-  results: [{text:'test', roomname: 'testroom', username:'gwbb'}]
-};
+  results: [{text: '1', roomname: 'testroom', username: 'gwbb'},
+    {text: '2', roomname: 'testroom', username: 'gwbb'},
+    {text: '3', roomname: 'testroom', username: 'gwbb'},
+    {text: '4', roomname: 'testroom', username: 'gwbb'},
+    {text: '5', roomname: 'testroom', username: 'gwbb'},
+    {text: '6', roomname: 'testroom', username: 'gwbb'}]};
 
 var requestHandler = function(request, response) {
   console.log('request', request);
@@ -37,7 +42,6 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  // console.log(request);
   // The outgoing status.
   var statusCode = 200;
 
@@ -49,23 +53,10 @@ var requestHandler = function(request, response) {
     'access-control-max-age': 10 // Seconds.
   };
   var headers = defaultCorsHeaders;
-  // var data = {
-  //   results: [{username: 'JSON'}]
-  // };
-
-  // From https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
-  // request.on('error', (err) => {
-  //   console.error(err);
-  //   response.statusCode = 400;
-  //   response.end();
-  // });
-  // response.on('error', (err) => {
-  //   console.error(err);
-  // });
   if (request.method === 'OPTIONS') {
     response.writeHead(200, headers);
     response.end();
-  } else if (request.method === 'POST' && request.url === '/classes/messages') {
+  } else if (request.method === 'POST' && pathname === '/classes/messages') {
     response.writeHead(201, headers);
     var body = [];
     request.on('data', (chunk) => {
@@ -76,52 +67,60 @@ var requestHandler = function(request, response) {
       let message = JSON.parse(body.toString());
       message['createdAt'] = new Date();
       data.results.push(message);
-      console.log(data);
     });
-    // body = JSON.parse(body);
-    // console.log('BODY', body);
-    // data.results.push(body.toString());
-    // request.pipe(response);
    
     response.end();
   } else if (request.method === 'GET' && pathname === ('/classes/messages')) {
     console.log('ASKED FOR MESSAGES!!!11!!!!1!11');
+    
+    let responseData = {results: []};
+    
+    if (query.order) {
+      if (query.order === '-createdAt') {
+        for (let i = data.results.length - 1;
+          i >= 0 && i >= data.results.length - RETURN_LIMIT;
+          i--) {
+          responseData.results.push(data.results[i]);
+        }
+      }
+    } else {
+      for (let i = 0;
+        i <= data.results.length && i <= RETURN_LIMIT;
+        i++) {
+        responseData.results.push(data.results[i]);
+      }
+    }
+    
     headers['Content-Type'] = 'application/json';
     response.writeHead(200, headers);
-    response.end(JSON.stringify(data));
+    response.end(JSON.stringify(responseData));
   } else {
     response.statusCode = 404;
     response.end();
   }
-  // End https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
 
-
-
-
-
-
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  // headers['Content-Type'] = 'application/json';
-  
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // console.log('BODY', body);
-  // response.end('hi there');
 };
+
+exports.requestHandler = requestHandler;
+
+// Tell the client we are sending them plain text.
+
+// You will need to change this if you are sending something
+// other than plain text, like JSON or HTML.
+// headers['Content-Type'] = 'application/json';
+
+
+// .writeHead() writes to the request line and headers of the response,
+// which includes the status and all headers.
+// response.writeHead(statusCode, headers);
+
+// Make sure to always call response.end() - Node may not send
+// anything back to the client until you do. The string you pass to
+// response.end() will be the body of the response - i.e. what shows
+// up in the browser.
+//
+// Calling .end "flushes" the response's internal buffer, forcing
+// node to actually send all the data over to the client.
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -133,4 +132,3 @@ var requestHandler = function(request, response) {
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
 
-exports.requestHandler = requestHandler;
